53e99a4dc0adbe0d7a3ab76e44f4af1085066ca1
Monica  Karasek <mlkarasek@gmail.com>
2019-11-22 21:48:50 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
+  renderRestaurants= () => {
+    return this.props.restaurants.map(restaurant => <Restaurant restaurant={restaurant} key={restaurant.id} deleteRestaurant={this.props.deleteRestaurant} />)
+  }
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.renderRestaurants()}
       </ul>
     );
   }
