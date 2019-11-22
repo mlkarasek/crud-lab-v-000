a2773b1fbb66957558f3d3e0bc565a5e19197811
Monica  Karasek <mlkarasek@gmail.com>
2019-11-22 21:50:01 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
+  renderReviews = () => {
+    const filteredReviews = this.props.reviews.filter(review => review.restaurantId == this.props.restaurantId)
+    return filteredReviews.map(review => <Review key={review.id} review={review} deleteReview={this.props.deleteReview}/>)
+  }
+
   render() {
+    console.log('in the reviews component', this.props.reviews)
     return (
       <ul>
-        Reviews
+        {this.renderReviews()}
       </ul>
     );
   }
